696a31d3a944635c268ada6b2ec646e1e58a7fdd
Ishanpatni123 <ishanpatni123@gmail.com>
2023-06-24 11:25:13 +0530

    first commit for sidebar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import SideBar from "./Components/sideBar";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import Overall from "./pages/Overall";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <SideBar>
+        <Routes>
+          <Route path="/Overall" element={<Overall />} />
+        </Routes>
+      </SideBar>
+    </Router>
   );
 }
 
---
+++ my-app/src/Components/sideBar.jsx
@@ -0,0 +1,191 @@
+import { NavLink } from "react-router-dom";
+import { FaBuilding , FaBars} from "react-icons/fa";
+import { MdMessage } from "react-icons/md";
+import { BiLogOut } from "react-icons/bi";
+import { MdLeaderboard } from "react-icons/md";
+import { SiCoveralls } from "react-icons/si";
+import { BsFillTrophyFill, BsFillFileEarmarkRuledFill} from "react-icons/bs";
+
+
+import { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import SidebarMenu from "./SidebarMenu";
+import '../../src/sidebar.css' 
+const routes = [
+  {
+    path: "/",
+    name: "Leaderboards",
+    icon: < MdLeaderboard/>,
+    subRoutes: [
+      {
+        path: "/Overall",
+        name: "Overall ",
+        icon: <SiCoveralls />,
+      },
+      {
+        path: "/s",
+        name: "Genre-1",
+        // icon: < />,
+      },
+      {
+        path: "/s",
+        name: "Genre-2",
+        // icon: < />,
+      },
+      {
+        path: "/s",
+        name: "Genre-3",
+        // icon: < />,
+      },
+    ],
+  },
+  {
+    path: "/s",
+    name: "Hostels",
+    icon: <FaBuilding />,
+  },
+  {
+    path: "/users",
+    name: "Rules",
+    icon: < BsFillFileEarmarkRuledFill/>,
+  },
+  
+  
+  {
+    path: "/s",
+    name: "GCs",
+    icon: <BsFillTrophyFill />,
+    subRoutes: [
+      {
+        path: "/s",
+        name: "Upcoming ",
+        // icon: < />,
+      },
+      {
+        path: "/s",
+        name: "Ongoing",
+        // icon: < />,
+      },
+      {
+        path: "/s",
+        name: "Past",
+        // icon: < />,
+      },
+    ],
+  },
+  {
+    path: "/s",
+    name: "Discussion forum",
+    icon: <MdMessage />,
+  },
+  {
+    path: "/s",
+    name: "Logout",
+    icon: <BiLogOut />,
+  },
+  
+  
+];
+
+const SideBar = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+
+  const showAnimation = {
+    hidden: {
+      width: 0,
+      opacity: 0,
+      transition: {
+        duration: 0.5,
+      },
+    },
+    show: {
+      opacity: 1,
+      width: "auto",
+      transition: {
+        duration: 0.5,
+      },
+    },
+  };
+
+  return (
+    <>
+      <div className="main-container">
+        <motion.div
+          animate={{
+            width: isOpen ? "200px" : "45px",
+
+            transition: {
+              duration: 0.5,
+              type: "spring",
+              damping: 10,
+            },
+          }}
+          className={`sidebar `}
+        >
+          <div className="top_section">
+            <AnimatePresence>
+              {isOpen && (
+                <motion.h1
+                  variants={showAnimation}
+                  initial="hidden"
+                  animate="show"
+                  exit="hidden"
+                  className="logo"
+                >
+                  Tech GC 23-24
+                </motion.h1>
+              )}
+            </AnimatePresence>
+
+            <div className="bars">
+              <FaBars onClick={toggle} />
+            </div>
+          </div>
+          <section className="routes">
+            {routes.map((route, index) => {
+              if (route.subRoutes) {
+                return (
+                  <SidebarMenu
+                    setIsOpen={setIsOpen}
+                    route={route}
+                    showAnimation={showAnimation}
+                    isOpen={isOpen}
+                  />
+                );
+              }
+
+              return (
+                <NavLink
+                  to={route.path}
+                  key={index}
+                  className="link"
+                  activeClassName="active"
+                >
+                  <div className="icon">{route.icon}</div>
+                  <AnimatePresence>
+                    {isOpen && (
+                      <motion.div
+                        variants={showAnimation}
+                        initial="hidden"
+                        animate="show"
+                        exit="hidden"
+                        className="link_text"
+                      >
+                        {route.name}
+                      </motion.div>
+                    )}
+                  </AnimatePresence>
+                </NavLink>
+              );
+            })}
+          </section>
+        </motion.div>
+
+        <main>{children}</main>
+      </div>
+    </>
+  );
+};
+
+export default SideBar;
