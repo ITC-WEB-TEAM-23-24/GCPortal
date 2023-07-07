3c026d02ca0363c1ca6bf442f351e0f6364d2c4e
Ishanpatni123 <ishanpatni123@gmail.com>
2023-07-07 12:03:38 +0530

    integration

--- my-app/src/pages/Overall.jsx
+++ my-app/src/pages/Overall.jsx
@@ -2,241 +2,82 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../Leaderboards.css";
 import AnimatedPage from "../AnimatedPage";
+import axios from "axios";
 
-function Overall() {
-  return (
-    <div className="bg">
-      <div className="leaderboard_heading">
-        <h2 data-text="Leaderboards">Leaderboards</h2>
-      </div>
-      <div className="animation">
-        <div className="list">
-          <ul className="listing">
-            <li class="list_item_on">
-              <Link to="/Overall">Overall</Link>
-            </li>
-            <li class="list_items">
-              <Link to="/Genre1">
-                Genre1
-                {/* <button class="clickable">
-                <span class="shadow"></span>
-                <span class="edge"></span>
-                <span class="front"> Genre1 </span>
-              </button> */}
-              </Link>
-            </li>
-            <li class="list_items">
-              <Link to="/Genre2">Genre2</Link>
-            </li>
-            <li class="list_items">
-              <Link to="/Genre3">Genre3</Link>
-            </li>
-          </ul>
+class Overall extends React.Component {
+  state = { details: [] };
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:8000")
+      .then((res) => {
+        const data = res.data;
+        this.setState({
+          details: data,
+        });
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+
+  render() {
+    return (
+      <div className="bg">
+        <div className="leaderboard_heading">
+          <h2 data-text="Leaderboards">Leaderboards</h2>
         </div>
-        <AnimatedPage>
-          <div className="name_score">
-            <ul className="name_score_listing">
-              <li className="name_score_style">Position</li>
-              <li className="name_score_style">Hostel image</li>
-              <li className="name_score_style">Hostel Name</li>
-              <li className="name_score_style">Score</li>
+        <div className="animation">
+          <div className="list">
+            <ul className="listing">
+              <li className="list_item_on">
+                <Link to="/Overall">Overall</Link>
+              </li>
+              <li className="list_items">
+                <Link to="/Genre1">Genre1</Link>
+              </li>
+              <li className="list_items">
+                <Link to="/Genre2">Genre2</Link>
+              </li>
+              <li className="list_items">
+                <Link to="/Genre3">Genre3</Link>
+              </li>
             </ul>
           </div>
-          <div className="first">
-            <div className="position">
-              <h4>1st</h4>
+          <AnimatedPage>
+            <div className="name_score">
+              <ul className="name_score_listing">
+                <li className="name_score_style">Position</li>
+                <li className="name_score_style">Hostel image</li>
+                <li className="name_score_style">Hostel Name</li>
+                <li className="name_score_style">Score</li>
+              </ul>
             </div>
-            <div className="image">
-              <img alt="img_hostel" />
-            </div>
-            <div className="name">
-              <h3 className="name text-dark">Hostel_number</h3>
-              <div className="span">Hostel_name</div>
-            </div>
+            {this.state.details.map((output, id) => (
+              <div key={id}>
+                <div className="first">
+                  <div className="position">
+                    <h4>1st</h4>
+                  </div>
+                  <div className="image">
+                    <img alt="img_hostel" />
+                  </div>
+                  <div className="name">
+                    <h3 className="name text-dark">{output.Hostel}</h3>
+                    <div className="span">Hostel_name</div>
+                  </div>
 
-            <div className="score">
-              <span>Score</span>
-            </div>
-          </div>
-          <div className="second">
-            <div className="position">
-              <h4>2nd</h4>
-            </div>
-            <div className="image">
-              <img alt="img_hostel" />
-            </div>
-            <div className="name">
-              <h3 className="name text-dark">Hostel_number</h3>
-              <div className="span">Hostel_name</div>
-            </div>
-
-            <div className="score">
-              <span>Score</span>
-            </div>
-          </div>
-          <div className="third">
-            <div className="position">
-              <h4>3rd</h4>
-            </div>
-            <div className="image">
-              <img alt="img_hostel" />
-            </div>
-            <div className="name">
-              <h3 className="name text-dark">Hostel_number</h3>
-              <div className="span">Hostel_name</div>
-            </div>
-
-            <div className="score">
-              <span>Score</span>
-            </div>
-          </div>
-          <div className="General">
-            <div className="position">
-              <h4>xth</h4>
-            </div>
-            <div className="image">
-              <img alt="img_hostel" />
-            </div>
-            <div className="name">
-              <h3 className="name text-dark">Hostel_number</h3>
-              <div className="span">Hostel_name</div>
-            </div>
-
-            <div className="score">
-              <span>Score</span>
-            </div>
-          </div>
-          <div className="General">
-            <div className="position">
-              <h4>xth</h4>
-            </div>
-            <div className="image">
-              <img alt="img_hostel" />
-            </div>
-            <div className="name">
-              <h3 className="name text-dark">Hostel_number</h3>
-              <div className="span">Hostel_name</div>
-            </div>
-
-            <div className="score">
-              <span>Score</span>
-            </div>
-          </div>
-          <div className="General">
-            <div className="position">
-              <h4>xth</h4>
-            </div>
-            <div className="image">
-              <img alt="img_hostel" />
-            </div>
-            <div className="name">
-              <h3 className="name text-dark">Hostel_number</h3>
-              <div className="span">Hostel_name</div>
-            </div>
-
-            <div className="score">
-              <span>Score</span>
-            </div>
-          </div>
-          <div className="General">
-            <div className="position">
-              <h4>xth</h4>
-            </div>
-            <div className="image">
-              <img alt="img_hostel" />
-            </div>
-            <div className="name">
-              <h3 className="name text-dark">Hostel_number</h3>
-              <div className="span">Hostel_name</div>
-            </div>
-
-            <div className="score">
-              <span>Score</span>
-            </div>
-          </div>
-          <div className="General">
-            <div className="position">
-              <h4>xth</h4>
-            </div>
-            <div className="image">
-              <img alt="img_hostel" />
-            </div>
-            <div className="name">
-              <h3 className="name text-dark">Hostel_number</h3>
-              <div className="span">Hostel_name</div>
-            </div>
-
-            <div className="score">
-              <span>Score</span>
-            </div>
-          </div>
-          <div className="General">
-            <div className="position">
-              <h4>xth</h4>
-            </div>
-            <div className="image">
-              <img alt="img_hostel" />
-            </div>
-            <div className="name">
-              <h3 className="name text-dark">Hostel_number</h3>
-              <div className="span">Hostel_name</div>
-            </div>
-
-            <div className="score">
-              <span>Score</span>
-            </div>
-          </div>
-          <div className="General">
-            <div className="position">
-              <h4>xth</h4>
-            </div>
-            <div className="image">
-              <img alt="img_hostel" />
-            </div>
-            <div className="name">
-              <h3 className="name text-dark">Hostel_number</h3>
-              <div className="span">Hostel_name</div>
-            </div>
-
-            <div className="score">
-              <span>Score</span>
-            </div>
-          </div>
-          <div className="General">
-            <div className="position">
-              <h4>xth</h4>
-            </div>
-            <div className="image">
-              <img alt="img_hostel" />
-            </div>
-            <div className="name">
-              <h3 className="name text-dark">Hostel_number</h3>
-              <div className="span">Hostel_name</div>
-            </div>
-
-            <div className="score">
-              <span>Score</span>
-            </div>
-          </div>
-          <div className="General">
-            <div className="position">
-              <h4>xth</h4>
-            </div>
-            <div className="image">
-              <img alt="img_hostel" />
-            </div>
-            <div className="name">
-              <h3 className="name text-dark">Hostel_number</h3>
-              <div className="span">Hostel_name</div>
-            </div>
-
-            <div className="score">
-              <span>Score</span>
-            </div>
-          </div>
-        </AnimatedPage>
+                  <div className="score">
+                    <span>{output.total}</span>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </AnimatedPage>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+
 export default Overall;
