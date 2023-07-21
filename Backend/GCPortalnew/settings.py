d7c31dd64113ecb663cd085bb04986ce8302d111
DeepakSilaych <deepaksilaych@gmail.com>
2023-07-21 08:49:21 +0530

    commit

--- Backend/GCPortalnew/settings.py
+++ Backend/GCPortalnew/settings.py
@@ -142,6 +142,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
