c4cfff263d6dd983bc7758f8eece341bad1678ae
Daksh-99 <22b2150@iitb.ac.in>
2023-07-23 20:02:58 +0530

    resolved same rank for same score issue, fixed create-gc API, added token authorization to all APIs

--- Backend/GCPortalnew/settings.py
+++ Backend/GCPortalnew/settings.py
@@ -40,10 +40,17 @@ INSTALLED_APPS = [
     'scorecard',
     'rest_framework',
     'corsheaders',
+    'rest_framework.authtoken'
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+}
+
 MIDDLEWARE = [
-    
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
