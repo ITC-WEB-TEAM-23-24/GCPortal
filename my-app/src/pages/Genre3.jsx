4aa2965ec16882febe7e7d842f512a59354f481b
DeepakSilaych <deepaksilaych@gmail.com>
2023-07-26 00:22:00 +0530

    deepak

--- my-app/src/pages/Genre3.jsx
+++ my-app/src/pages/Genre3.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "../Leaderboards.css";
+import "../assets/css/leaderboard.css";
 import ScoreboardAnimation from "../Animation";
 import axios from "axios";
 
